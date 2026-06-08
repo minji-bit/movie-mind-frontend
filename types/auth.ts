9eff1f0a3f6a5b79ae7ef55ffd0b11d4f036c55d
jeon min ji <jeon320@naver.com>
2026-06-08 14:12:34 +0900

    chore: types/auth.ts 수정

--- types/auth.ts
+++ types/auth.ts
@@ -1,4 +1,25 @@
-export type Auth = {
+export interface LoginRequest {
   email: string;
   password: string;
-};
+}
+export interface LoginResponse {
+  id: string;
+  email: string;
+  nickname: string;
+  accessToken: string;
+}
+export interface SignupRequest {
+  email: string;
+  password: string;
+  nickname: string;
+}
+export interface SignupResponse {
+  id: string;
+  email: string;
+  nickname: string;
+}
+export interface MeResponse {
+  id: string;
+  email: string;
+  nickname: string;
+}
