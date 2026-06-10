e12a3d46e7a7e4cb6334cba420615f3210c30edf
Min <jeon320@naver.com>
2026-06-10 10:29:10 +0900

    feat: 로그인 화면만 생성(백엔드 연결 필요)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,47 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email, password);
+    //로그인 성공시 토큰 저장
+    localStorage.setItem("accessToken", "1234567890");
+    //로그인 성공시
+    router.push("/reviews");
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="이메일"
+          onChange={handleEmailChange}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+        <button type="submit">로그인</button>
+      </form>
+    </div>
+  );
 }
