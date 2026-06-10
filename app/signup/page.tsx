8089b02953411a10e0addcc47ce0f40ab2ea8f1d
Min <jeon320@naver.com>
2026-06-10 10:17:44 +0900

    feat: 회원가입 화면만 생성(백엔드연결 필요)

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,66 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function SignupPage() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const router = useRouter();
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordConfirmChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setPasswordConfirm(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(email, nickname, password, passwordConfirm);
+    //로그인 성공시
+    router.push("/login");
+  };
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="이메일"
+          onChange={handleEmailChange}
+          value={email}
+        />
+        <input
+          type="text"
+          placeholder="닉네임"
+          onChange={handleNicknameChange}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          onChange={handlePasswordChange}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호 확인"
+          onChange={handlePasswordConfirmChange}
+        />
+        <button type="submit">회원가입</button>
+      </form>
+    </div>
+  );
 }
