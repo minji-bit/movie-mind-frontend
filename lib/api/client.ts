1ac8b1fa58dc19bd9b29c1788ce736fa2b30e3fd
jeon min ji <jeon320@naver.com>
2026-06-11 09:22:56 +0900

    fix: fetch 보낼때 header 추가

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,9 +1,17 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://localhost:4000";
 
 export async function apiFetch(url: string, options?: RequestInit) {
-  const response = await fetch(`${BASE_URL}${url}`, options);
+  const response = await fetch(`${BASE_URL}${url}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options?.headers,
+    },
+  });
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    throw new Error(
+      `HTTP error! status: ${response.status}, message: ${response.statusText}`,
+    );
   }
   return response.json();
 }
