bd534b9dc2b84c0acb1f5d6bb258c9a57ccc5c53
jeon min ji <jeon320@naver.com>
2026-06-15 14:45:39 +0900

    chore: header 수정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,11 +2,24 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div>
-      <Link href="/">Movie Mind</Link>
-      <div>리뷰목록</div>
-      <div>로그인</div>
-      <div>회원가입</div>
-    </div>
+    <header className="flex items-center justify-between px-8 h-16 bg-gray-200">
+      <Link href="/" className="text-2xl font-bold">
+        Movie Mind
+      </Link>
+      <nav className="flex gap-4">
+        <Link href="/reviews" className="hover:text-blue-500">
+          리뷰목록
+        </Link>
+        <Link href="/reviews/new" className="hover:text-blue-500">
+          리뷰작성
+        </Link>
+        <Link href="/login" className="hover:text-blue-500">
+          로그인
+        </Link>
+        <Link href="/signup" className="hover:text-blue-500">
+          회원가입
+        </Link>
+      </nav>
+    </header>
   );
 }
